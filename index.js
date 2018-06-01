5401ab48e539dafe882c6436ba92acde579da110
pirjotuulia <pirjoleppanen@nic.fi>
2018-06-01 11:49:00 +0300

    3.4

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ app.get('/info', (request, response) => {
     response.send(info)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response)=> {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
