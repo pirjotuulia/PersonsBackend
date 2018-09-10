0614618f0a0b083d07ad76c19ee34c83044b6779
pirjotuulia <pirjoleppanen@nic.fi>
2018-09-10 09:55:38 +0300

    korjauksia

--- index.js
+++ index.js
@@ -10,58 +10,58 @@ morgan.token('response', function (req, res) { return JSON.stringify(req.body) }
 app.use(morgan(':method :url :response :status :res[content-length] - :response-time ms'))
 
 let persons = [
-        {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-        },
-        {
-            "name": "Martti Tienari",
-            "number": "040-123456",
-            "id": 2
-        },
-        {
-            "name": "Arto Järvinen",
-            "number": "040-123456",
-            "id": 3
-        },
-        {
-            "name": "Lea Kutvonen",
-            "number": "040-123456",
-            "id": 4
-        }
-    ]
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Martti Tienari",
+        "number": "040-123456",
+        "id": 2
+    },
+    {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 3
+    },
+    {
+        "name": "Lea Kutvonen",
+        "number": "040-123456",
+        "id": 4
+    }
+]
 
 app.get('/', (request, response) => {
-        response.send('wow')
-    })
+    response.send('wow')
+})
 
 app.get('/api/persons', (request, response) => {
-        response.json(persons)
-    })
+    response.json(persons)
+})
 
 app.get('/info', (request, response) => {
-        const amount = persons.length
-        const date = new Date()
-        const info = `puhelinluettelossa on ${amount} henkilön tiedot <br> ${date}`
-        response.send(info)
-    })
+    const amount = persons.length
+    const date = new Date()
+    const info = `puhelinluettelossa on ${amount} henkilön tiedot <br> ${date}`
+    response.send(info)
+})
 
 app.get('/api/persons/:id', (request, response) => {
-        const id = Number(request.params.id)
-        const person = persons.find(p => p.id === id)
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
 app.delete('/api/persons/:id', (request, response) => {
-        const id = Number(request.params.id)
-        persons = persons.filter(p => p.id !== id)
-        response.status(204).end()
-    })
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
 
 const generateId = () => {
     const maxId = persons.length > 0 ? persons.map(p => p.id).sort().reverse()[0] : 0
@@ -84,7 +84,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
