5e985eda24d0761c5aa07bb15cc5d603a97ce6fe
pirjotuulia <pirjoleppanen@nic.fi>
2018-09-10 10:42:36 +0300

    use build

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
 morgan.token('response', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :response :status :res[content-length] - :response-time ms'))
-app.use(express.static('build'))
 
 let persons = [
     {
